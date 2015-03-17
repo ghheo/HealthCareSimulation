23072f1ef3fd9b6a107ac9556335a5f06c7fdb28
alexgomes09 <alex_09hg@yahoo.com>
2015-03-17 02:49:23 -0400

    gitignore modified

---
+++ Assets/Scripts/SimulationManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimulationManager : MonoBehaviour {
+
+	ArrayList waitingPatients;
+	Queue triagePatients,treatMentPatients;
+
+
+	void Start () {
+		waitingPatients = new ArrayList();
+		Patient pat1 = new Patient(PatientType.Emergency);
+		waitingPatients.Add(pat1);
+
+		print (waitingPatients.Count);
+	}
+	
+	void Update () {
+		
+	}
+}
