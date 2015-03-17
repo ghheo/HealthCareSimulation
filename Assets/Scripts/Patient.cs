67620add906be4faa3cac71784af42f7145364dc
alexgomes09 <alex_09hg@yahoo.com>
2015-03-17 02:02:54 -0400

    Patient enum added

---
+++ Assets/Scripts/Patient.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PatientType{
+	Reneging,
+	Normal,
+	Emergency
+}
+
+public class Patient {
+
+	public string patientType;
+
+
+
+	public Patient(PatientType patType){
+		this.patientType = patType.ToString();
+	}
+
+
+
+}
+
+
---
+++ Assets/Scripts/test1.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test1 : MonoBehaviour {
+
+	void Start () {
+		Patient pat = new Patient(PatientType.Emergency);
+		print(pat.patientType);
+	}
+	
+	void Update () {
+		
+	}
+}
